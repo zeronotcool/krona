3ef1b3fb28aba28c1024797bc8ac16018e616695
pasota2 <pasota@gmail.com>
2024-05-28 00:18:52 +0200

    Lee la fecha.

--- krona.js
+++ krona.js
@@ -4,6 +4,8 @@
 
 */
 import axios from 'axios';
+import cheerio from 'cheerio';
+import fs from 'fs';
 
 /*
 
@@ -14,7 +16,7 @@ import axios from 'axios';
 const urlVolumen = 'https://dc.fandom.com/wiki/Action_Comics_Vol_1_'
 //const urlVolumen = 'https://dc.fandom.com/wiki/Action_Comics_Vol_2_'
 
-let numeroInicial = 1;  // número en que empezamos a extraer información
+let numeroInicial = 655;  // número en que empezamos a extraer información
 let numeroFinal = 0;    // número en el que dejamos de extraer información (0 para llegar al último)
 
 /*
@@ -26,18 +28,39 @@ let numeroFinal = 0;    // número en el que dejamos de extraer información (0 
 const fetchData = async (url) => {
 
     try {
+        // Obtiene el código HTML
         const response = await axios.get(url);
 
+        // Si la URL no existe detiene el programa
         if (response.status === 404) {
             console.log(`La página ${url} no se encontró. Abortando.`)
             return;
+        } else {
+
         }
 
-        console.log('Status:', response.status);
-        console.log('Data:', response.data);
+        //console.log('Status:', response.status);
+        //console.log('Data:', response.data);
+
+        // Carga el contenido de la web
+        const $ = cheerio.load(response.data);
+        const htmlContent = $.html();
+
+        // Vuelca el contenido a un fichero output.html
+        fs.writeFile('output.html', htmlContent, 'utf8', (err) => {
+            if (err) {
+                console.error('Error al escribir el fichero:', err);
+            } else {
+                console.log('Fichero escrito correctamente.');
+            }
+        });
+
+        // Obtiene la fecha del comic
+        const dateText = $('a[data-tracking-label="categories-top-more-41"]').text();
+        console.log(dateText);
 
     } catch (error) {
-        console.log('Se produjo un error');
+        console.log(`Se produjo un error: ${error}`);
     }
 
 }
