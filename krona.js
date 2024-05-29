0f3a08cbb31b1243a86254e2e4d8df828839bc8d
pasota2 <pasota@gmail.com>
2024-05-29 10:56:16 +0200

    Extrae autores y fechas correctamente.

--- krona.js
+++ krona.js
@@ -16,7 +16,7 @@ import fs from 'fs';
 const urlVolumen = 'https://dc.fandom.com/wiki/Action_Comics_Vol_1_'
 //const urlVolumen = 'https://dc.fandom.com/wiki/Action_Comics_Vol_2_'
 
-let numeroInicial = 655;  // número en que empezamos a extraer información
+let numeroInicial = 1;  // número en que empezamos a extraer información
 let numeroFinal = 0;    // número en el que dejamos de extraer información (0 para llegar al último)
 
 /*
@@ -26,7 +26,6 @@ let numeroFinal = 0;    // número en el que dejamos de extraer información (0 
 */
 
 const fetchData = async (url) => {
-
     try {
         // Obtiene el código HTML
         const response = await axios.get(url);
@@ -47,6 +46,7 @@ const fetchData = async (url) => {
         const htmlContent = $.html();
 
         // Vuelca el contenido a un fichero output.html
+        /*
         fs.writeFile('output.html', htmlContent, 'utf8', (err) => {
             if (err) {
                 console.error('Error al escribir el fichero:', err);
@@ -54,16 +54,67 @@ const fetchData = async (url) => {
                 console.log('Fichero escrito correctamente.');
             }
         });
+        */
 
         // Obtiene la fecha del comic
-        const dateText = $('a[data-tracking-label="categories-top-more-41"]').text();
-        console.log(dateText);
+        //const writerText = $('a[data-tracking-label="categories-top-more-4"]').text();
+        //const pencillerText = $('a[data-tracking-label="categories-top-more-5"]').text();
+        //const dateText = $('a[data-tracking-label="categories-top-more-41"]').text();
+        const dateText = extractDate(response.data);
+        const writersText = extractField('Writers', $);
+        const pencillersText = extractField('Pencilers', $);        
+
+        console.log('Fecha: ' + dateText)
+        console.log('Escritores: ' + writersText);
+        console.log('Dibujantes: ' + pencillersText);
+        //console.log(dateText);
 
     } catch (error) {
         console.log(`Se produjo un error: ${error}`);
     }
 
 }
+
+/*
+
+    Función para parsear autores
+
+*/
+
+const extractField = (label, $) => {
+    const h3Element = $(`h3.pi-data-label:contains("${label}")`);
+    if (h3Element.length > 0) {
+        const divElement = h3Element.next('div.pi-data-value');
+        const anchorElements = divElement.find('a');
+        if (anchorElements.length > 0) {
+            // Eliminar duplicados y procesar cada enlace por separado
+            const uniqueValues = [...new Set(anchorElements.map((index, element) => $(element).text().trim()))];
+            return uniqueValues.join(',');
+        } else {
+            return '';
+        }
+    } else {
+        return '';
+    }
+};
+
+function extractDate(html) {
+    // Cargar el HTML con Cheerio
+    const $ = cheerio.load(html);
+  
+    // Seleccionar el segundo <h2> elemento que contiene la fecha
+    const dateElement = $('h2').filter(function () {
+      // Filtrar por el contenido de enlaces dentro del <h2> que contengan una coma (indicativo de mes y año)
+      return $(this).find('a').length >= 2 && $(this).text().includes(',');
+    });
+  
+    // Extraer y concatenar el texto de los enlaces dentro del <h2>
+    const month = dateElement.find('a').first().text().trim();
+    const year = dateElement.find('a').last().text().trim();
+  
+    // Devolver la fecha en el formato deseado
+    return `${month}, ${year}`;
+  }
 
 /*
 
